056c7e34db51fab8272687a199a0fc4849213f6b
Juan Ocho <juaneme8@gmail.com>
2022-03-29 22:38:28 -0300

    About completed

--- components/About.tsx
+++ components/About.tsx
@@ -8,42 +8,42 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <section id='about' className='min-h-screen md:min-h-[68vh] lg:min-h-screen w-3/4 mx-auto '>
+    <section id='about' className='min-h-screen w-3/4 mx-auto pt-4'>
       <h5 className='text-center'>Get to Know</h5>
       <h2 className='text-center text-xl text-sky-400 font-semibold'>About Me</h2>
 
-      <div className='grid grid-cols-[35%_50%] gap-[15%] mt-8'>
-        <div className=' bg-gradient-to-r from-transparent via-sky-400 to-transparent w-full aspect-square rounded-3xl '>
+      <div className='grid md:grid-cols-[30%_65%] grid-cols-1 md:gap-[5%] mt-2 md:mt-8'>
+        <div className='bg-gradient-to-r from-transparent via-sky-400 to-transparent w-full mx-auto mt-8 mb-10 aspect-square rounded-3xl'>
           <div className='relative rotate-12 hover:rotate-0 transition duration-300 ease-in w-full aspect-square rounded-3xl truncate bg-gradient-to-tr'>
             <Image src='/assets/images/me-about.jpg' alt='About Me' layout='fill' />
           </div>
         </div>
         <div className='flex flex-col justify-center'>
-          <div className='flex flex-col md:flex-row gap-6 justify-center'>
-            <article className='bg-sky-400/40 border border-transparent rounded-lg py-8 px-6 text-gray-800 flex justify-center flex-col items-center gap-3 hover:bg-transparent hover:border-sky-400/40 transition-colors ease-in transition-duration-300'>
+          <div className='flex flex-row gap-3 justify-center flex-wrap content-center'>
+            <article className='basis-2/5 md:basis-auto bg-sky-400/40 border border-transparent rounded-lg p-5 text-gray-800 flex justify-center flex-col items-center gap-3 hover:bg-transparent hover:border-sky-400/40 transition-colors ease-in transition-duration-300'>
               <FaAward className='h-6 w-6 text-sky-400' />
               <h5 className='text-white'>Experience</h5>
               <small className='text-xs text-neutral-50/80'>3+ Years Working</small>
             </article>
-            <article className='bg-sky-400/40 border border-transparent rounded-lg py-8 px-6 text-gray-800 flex justify-center flex-col items-center gap-3 hover:bg-transparent hover:border-sky-400/40 transition-colors ease-in transition-duration-300'>
+            <article className='basis-2/5 md:basis-auto bg-sky-400/40 border border-transparent rounded-lg p-5 text-gray-800 flex justify-center flex-col items-center gap-3 hover:bg-transparent hover:border-sky-400/40 transition-colors ease-in transition-duration-300'>
               <FiUsers className='h-6 w-6 text-sky-400' />
               <h5 className='text-white'>Clients</h5>
               <small className='text-xs text-neutral-50/80'>200+ Worldwide</small>
             </article>
-            <article className='bg-sky-400/40 border border-transparent rounded-lg py-8 px-6 text-gray-800 flex justify-center flex-col items-center gap-3 hover:bg-transparent hover:border-sky-400/40 transition-colors ease-in transition-duration-300'>
+            <article className='basis-2/5 md:basis-auto bg-sky-400/40 border border-transparent rounded-lg p-5 text-gray-800 flex justify-center flex-col items-center gap-3 hover:bg-transparent hover:border-sky-400/40 transition-colors ease-in transition-duration-300'>
               <VscFolderLibrary className='h-6 w-6 text-sky-400' />
               <h5 className='text-white'>Projects</h5>
               <small className='text-xs text-neutral-50/80'>80+ Completed</small>
             </article>
           </div>
 
-          <p className='mt-8 mb-11 text-neutral-50/80'>
+          <p className='mt-4 mb-6 md:mt-8 md:mb-12 text-neutral-50/80 text-center md:text-justify'>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, sunt eum? Quasi sunt sapiente ea deleniti
             commodi amet magni nesciunt accusamus. Maxime vitae culpa esse reiciendis, itaque quam doloribus provident.
           </p>
           <a
             href='#contact'
-            className='text-gray-800 bg-sky-400 hover:bg-gray-800  rounded-full px-4 py-2 border border-gray-800 hover:border hover:text-white hover:border-white transition duration-300 ease-in max-w-max mx-auto'
+            className='text-gray-800 bg-sky-400 hover:bg-gray-800  rounded-full px-4 py-2 border border-gray-800 hover:border hover:text-white hover:border-white transition duration-300 ease-in max-w-max mx-auto md:mr-auto'
           >
             Let&apos; talk!
           </a>
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,15 +6,15 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className='md:min-h-[68vh] lg:min-h-screen min-h-screen w-3/4 md:pt-14 pt-8 text-center mx-auto relative'>
+    <header className='min-h-screen w-3/4 md:pt-14 pt-8 text-center mx-auto relative'>
       <h5 className='font-light'>Hello I&apos;m</h5>
       <h1 className='font-semibold md:text-4xl text-2xl'>Juan Lauria</h1>
       <h5 className='text-neutral-50'>Fullstack Developer</h5>
       <CallToAction />
 
-      <div className='absolute left-1/2 -translate-x-1/2 bg-gradient-to-b from-sky-400 to-transparent pt-8 px-4 rounded-t-full mt-8'>
-        <div className='h-64 w-56 mx-auto truncate'>
-          <Image src='/assets/images/me.png' alt='me' width='288' height='384' objectFit='contain' />
+      <div className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-gradient-to-b from-sky-400 to-transparent pt-8 px-4 rounded-t-full mt-8'>
+        <div className='h-82 w-82 md:h-64 md:w-56 mx-auto truncate'>
+          <Image src='/assets/images/me.png' alt='me' width='288' height='500' objectFit='fill' />
         </div>
       </div>
 
